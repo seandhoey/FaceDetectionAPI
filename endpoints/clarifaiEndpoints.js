70bfe2b27eef1e7daaa4e6967c3cad3da016f046
seandhoey <seanhoey@live.ca>
2023-07-03 13:26:08 -0600

    test

--- endpoints/clarifaiEndpoints.js
+++ endpoints/clarifaiEndpoints.js
@@ -5,10 +5,7 @@ export async function postClarifaiFaceDetection(req, res) {
   console.log("\n>>>BODY: ", req.body);
   const { url, height, width } = req.body;
   const CLARIFAIURL = process.env.CLARIFAI_URL;
-  console.log('user1', process.env.CLARIFAI_USERID)
   try {
-    console.log('url', CLARIFAIURL);
-    console.log('user2', process.env.CLARIFAI_USERID)
     const response = await fetch(CLARIFAIURL, generateClarifaiRequest(url));
     console.log('response', response);
     const jsonResponse = await response.json();
@@ -29,6 +26,7 @@ function generateClarifaiRequest(url) {
   const PAT_KEY = process.env.CLARIFAI_PATKEY;
   const USER_ID = process.env.CLARIFAI_USERID;
   const APP_ID = process.env.CLARIFAI_APPID;
+  console.log('appid', APP_ID);
 
   return {
     method: 'POST',
