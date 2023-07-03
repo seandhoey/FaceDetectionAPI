aaad2544950cc44a007d53607b48c72f4657c8ff
seandhoey <seanhoey@live.ca>
2023-07-03 12:31:22 -0600

    cors fix

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-if (!PRODUCTION) app.use(cors());
+app.use(cors()); // Shouldn't allow this in prod
 app.listen(PORT, () => {
   console.log("Server is listening on port " + PORT);
 });
